d35a6f21fa502019390e33d758104f957832eed9
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-11-11 02:21:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-
+export default {
+  PORT: process.env.PORT || 8080,
+  PRICE_CATALOG_URL: process.env.PRICE_CATALOG_URL || "https://bassam-customs-calculator.onrender.com/assets/prices_catalog.json",
+  CALCULATOR_URL: process.env.CALCULATOR_URL || "https://bassam-customs-calculator.onrender.com/index.html",
+  FX_YER_PER_USD: parseFloat(process.env.FX_YER_PER_USD || "910"),
+};
